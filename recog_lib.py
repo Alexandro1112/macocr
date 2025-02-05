b4d19b4ceab75d9da4758f382bc73996b0e68e19
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2025-02-05 21:23:11 +0300

    Create recog_lib.py

---
+++ recog_lib.py
@@ -0,0 +1,33 @@
+import Vision
+
+
+class Recognition:
+    def get_info_from_image(self, img, info):
+        self.info = info
+
+        def handle(request, error):
+            self.output = []
+            for result in request.results():
+                if isinstance(result, Vision.VNRecognizedTextObservation):
+                    for texts in result.topCandidates_(1):
+                        if self.info == 'text':
+                            self.output.append(texts.string())
+                        if self.info == 'coord':
+                            abst_position = result.boundingBox()
+                            self.output.append(abst_position)
+                        else:
+                            return None
+
+        def recognize(img):
+            image = Vision.NSImage.alloc().initWithContentsOfFile_(img)
+            if image is None:
+                raise FileNotFoundError('Failed to load image..')
+            rep = image.representations()[0]
+            cg_image = rep.CGImage()
+            req_handler = Vision.VNImageRequestHandler.alloc().initWithCGImage_options_(cg_image, None)
+            request = Vision.VNRecognizeTextRequest.alloc().initWithCompletionHandler_(handle)
+            req_handler.performRequests_error_([request], None)
+        recognize(img)
+
+    def return_results(self):
+        return self.output
