458de14ee4d53bad18d44004c4ff1ffed3214d18
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2025-02-14 12:23:16 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,18 +1,5 @@
 from .recog_lib import Recognition
-from langcodes import Language
-import CoreAudio
 
 
-def supported_langs():
-    """
-    get all ISO Languages codes and convert it from ISO 639 to full name.
-    :return:
-    """
-    _list = []
-    for lang in CoreAudio.CFLocaleCopyISOLanguageCodes():
-        _list.append(Language(lang).language_name())
-    return _list
-
-
-__all__ = [Recognition, supported_langs]
+__all__ = [Recognition, ]
 
