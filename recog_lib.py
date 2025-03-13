fd6caa92ab543646a6311562ae31b39edc789242
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2025-03-13 22:47:51 +0300

    Update recog_lib.py
    added the ability to accept as an image a numpy array that contains an image.

--- recog_lib.py
+++ recog_lib.py
@@ -5,8 +5,12 @@ import Quartz
 import Cocoa
 import re
 from typing import Dict, SupportsFloat, SupportsInt, List, AnyStr, Tuple, Literal
+import numpy
 import objc
+import cv2
 from collections.abc import Iterable
+import io
+from PIL import Image
 
 
 class Recognition:
@@ -46,7 +50,7 @@ class Recognition:
             metadata = item.__metadata__()['arguments']
             signature = b''.join(item['type'] for item in metadata)
             # b'@:@' mean that function return object not equal None;
-            if b'@:@' not in signature or all(signature in symbols for symbols in objc._objc.splitSignature(b'@:@')):
+            if b'@:@' not in signature or all(signature in symbols for symbols in objc.splitSignature(b'@:@')):
                 raise ValueError
         except AttributeError:
             raise AttributeError('Can not get access to %s' % repr(item))
@@ -71,7 +75,7 @@ class Recognition:
             }
 
         self.orientations = {
-            'default': Quartz.kCGImagePropertyOrientationUp & -2,
+            'default': 0,
             'up': Quartz.kCGImagePropertyOrientationUp,
             'down': Quartz.kCGImagePropertyOrientationDown,
             'left': Quartz.kCGImagePropertyOrientationLeft,
@@ -87,8 +91,7 @@ class Recognition:
             Create handler for accept recognized text.
             """
             if error is not None:
-                raise Exception(
-                    f"Code : {hex(error.code())!r}", error.localizedDescription())
+                raise Exception(error)
             del error
 
             self.all = {}
@@ -101,13 +104,24 @@ class Recognition:
                 for i in range(len(values)):
                     bound_box = [_xy for _xy in
                                  (values[i].x, values[i].y, values.size.width, values.size.height)]
-                    x, y, w, h = bound_box
-                    x_1 = x * self.width
-                    y_2 = (1 - y) * self.height
-                    x_2 = w * self.width
-                    y_1 = h * self.height
+                    if isinstance(img, str):
+                        x, y, w, h = bound_box
+                        x_1 = x * self.width
+                        y_2 = (1 - y) * self.height
+                        x_2 = w * self.width
+                        y_1 = h * self.height
 
-                    return x_1, y_2, x_2, y_1
+                        return x_1, y_2, x_2, y_1
+
+                    elif isinstance(img, numpy.ndarray):
+
+                        x, y, w, h = bound_box
+                        x_1 = x * img.shape[1]
+                        y_2 = (1 - y) * img.shape[0]
+                        x_2 = w * img.shape[1]
+                        y_1 = h * img.shape[0]
+
+                        return x_1, y_2, x_2, y_1
 
             if not isinstance(request.results(), Iterable) and recognition_interest is not None:
                 self.output_txt.append([])
@@ -116,7 +130,6 @@ class Recognition:
                 return
 
             for result in request.results():
-
                 if isinstance(result, Vision.VNRecognizedTextObservation):
                     for text in result.topCandidates_(1):
                         if len(self.info.split('+')) < 3:
@@ -152,37 +165,57 @@ class Recognition:
              the file path for Cyrillic characters, initializes image handling, sets recognition parameters,
               and manages errors during the recognition process.
             """
-            pattern = r'[а-яА-ЯёЁ]'
-            if bool(var := re.search(pattern, file)):
-                raise SyntaxError(
-                    f'The path to the file must\'t contain Cyrillic characters started on {var.start() + 1!r}.'
-                )
-
-            try:
-                image = Vision.NSImage.alloc().initWithContentsOfFile_(
-                    Cocoa.CFSTR(file)
-                ).TIFFRepresentation()
-                size = Vision.NSImage.alloc().initWithContentsOfFile_(Cocoa.CFSTR(img)).size()
-
-                self.width = int(size.width)
-                self.height = int(size.height)
-
-            except AttributeError:
-                raise FileNotFoundError(f'Failed to load image') from None
-
-            cg = Cocoa.NSBitmapImageRep.imageRepWithData_(
-                image
-            ).CGImage()
-
             try:
                 orient = self.orientations[img_orientation]
             except KeyError:
                 raise ValueError(
-                    f'Orientation must be {repr((*self.orientations.keys(), ))}'
+                    f'Orientation must be {repr((*self.orientations.keys(),))}'
                 )
-            req_handler = Vision.VNImageRequestHandler.alloc().initWithCGImage_orientation_options_(
-                cg, orient, None
-            )
+
+            if isinstance(file, str):
+                if bool(var := re.search(r'[а-яА-ЯёЁ]', file)):
+                    raise SyntaxError(
+                        f'The path to the file must\'t contain Cyrillic characters started on {var.start() + 1!r}.'
+                    )
+                try:
+                    image = Vision.NSImage.alloc().initWithContentsOfFile_(
+                        Cocoa.CFSTR(file)).TIFFRepresentation()
+                    size = Vision.NSImage.alloc().initWithContentsOfFile_(Cocoa.CFSTR(img)).size()
+
+                    self.width = int(size.width)
+                    self.height = int(size.height)
+
+                except AttributeError:
+                    raise FileNotFoundError(f'Failed to load image {img.__repr__()}') from None
+
+                cg = Cocoa.NSBitmapImageRep.imageRepWithData_(
+                    image
+                ).CGImage()
+
+                req_handler = Vision.VNImageRequestHandler.alloc().initWithCGImage_orientation_options_(
+                    cg, orient, None
+                )
+
+            elif isinstance(file, numpy.ndarray):
+                def numpy_array_to_ns_data(image_array):
+                    """Convert a NumPy array to NSData."""
+                    pil_image = Image.fromarray(image_array)
+                    with io.BytesIO() as output:
+                        pil_image.save(output, format='PNG', save_all=True)
+                        data = output.getvalue()
+                    return Cocoa.NSData.dataWithBytes_length_(data, len(data))
+
+                rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
+
+                image_bytes = numpy_array_to_ns_data(rgb)
+
+                ns_data_image = Cocoa.NSData.dataWithBytes_length_(image_bytes, len(image_bytes))
+
+                req_handler = Vision.VNImageRequestHandler.alloc().initWithData_orientation_options_(
+                    ns_data_image, orient, None)
+            else:
+                raise TypeError('Image can be a NumPy array with data of image, or path to file.')
+
             self.request = Vision.VNRecognizeTextRequest.alloc().initWithCompletionHandler_(completion_handler_)
             self.request.setRevision_error_(Vision.VNRecognizeTextRequestRevision3, None)
 
@@ -191,7 +224,7 @@ class Recognition:
                     self.request.setRegionOfInterest_(
                         Cocoa.CGRect(*recognition_interest)
                     )
-                except objc.internal_error:
+                except objc._objc.internal_error:
                     raise ValueError(f'recognition_interest has wrong type or values.')
 
             elif recognition_interest is None:
@@ -207,10 +240,12 @@ class Recognition:
 
             if not self.lang == 'en-US':
                 self.request.setRecognitionLanguages_(lang)
+
             if self.use_CPU is True:
-                self.request.setUsesCPUOnly_(Vision.kCFBooleanTrue)
+                self.request.setUsesCPUOnly_(Vision.kCFBooleanTrue) # noqa
+
             if recognition_level is not None:
-                self.request.setRecognitionLevel_(self.levels[recognition_level] if not 1 or 0 else 0)
+                self.request.setRecognitionLevel_(self.levels[recognition_level] if not (1 or 0) else 0)
 
             if args:
                 for (keys, values) in args.items():
@@ -244,27 +279,26 @@ class Recognition:
                         raise AttributeError
 
             with objc.autorelease_pool():
-                if req_handler.isMemberOfClass_(Vision.VNImageRequestHandler):
-                    req_handler.performRequests_error_([self.request], None)
+                req_handler.performRequests_error_([self.request], None) # noqa
 
         recognize(img)
 
-    @objc.python_method
     def return_results(self) -> Dict[AnyStr, SupportsInt] | List[SupportsFloat]:
         """I created this method because when we try to return any data through a handle, we get an error.
          The Objc method cannot return or save any values."""
+
         if not self.all:
             try:
-                return [sublist for sublist in [self.output_txt, self.output_crd, self.output_cnf] if sublist]
+                return [sublist for sublist in
+                        [self.output_txt, self.output_crd, self.output_cnf] if sublist]
             except IndexError:
                 return []
-
-        else:
-            return self.all
+        return self.all
 
     def text_lang(self):
         return self.request.recognitionLanguages()
 
+    @property
     def supported_languages(self):
         return self.request.supportedRecognitionLanguagesAndReturnError_(None)[0]
 
